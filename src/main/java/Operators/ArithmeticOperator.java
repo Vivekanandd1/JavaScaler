da267250f6bd310482fda5c8388732c49cac907c
vivekanandd1 <vivekanandd1@live.com>
2023-08-01 20:41:34 +0530

    Initial code

---
+++ src/main/java/Operators/ArithmeticOperator.java
@@ -0,0 +1,16 @@
+package Operators;
+
+public class ArithmeticOperator {
+
+    public static void main(String[] args) {
+        int a =10;
+        int b = 3;
+
+        System.out.println("addition="+ (a+b));
+        System.out.println("Multiplication="+ a*b);
+        System.out.println("division="+ a/b);
+        System.out.println("division="+ (a/(float) b));
+        System.out.println("Subtraction="+ (a-b));
+        System.out.println("modulo="+ (a%b));
+    }
+}
---
+++ src/main/java/Operators/NotOperator.java
@@ -0,0 +1,14 @@
+package Operators;
+
+public class NotOperator {
+    public static void main(String[] args) {
+        boolean flag = false;
+
+        if (flag){
+            System.out.println("Hey there");
+        }
+        else{
+            System.out.println("statement was false");
+        }
+    }
+}
